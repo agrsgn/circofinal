8385f0b4dabe4ef651af91b2b337930f0a2b6a07
agrsgn <37484409+agrsgn@users.noreply.github.com>
2025-05-21 19:07:34 -0300

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ import { bootstrapCameraKit } from '@snap/camera-kit';
 
   const lens = await cameraKit.lensRepository.loadLens(
     'b4c3d760-7deb-49b4-a8c8-632fd4994c45',
-    'b9635300-8c81-4d66-a702-660f054ff101'
+    'd00499b2-db28-46f2-bcb3-7ea5057fb704'
   );
 
   await session.applyLens(lens);
