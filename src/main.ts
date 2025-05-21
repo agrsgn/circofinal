6b3de0e9774568d084fd5420f4d74c3cb1dfee88
agrsgn <37484409+agrsgn@users.noreply.github.com>
2025-05-21 19:26:37 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapCameraKit } from '@snap/camera-kit';
 
 (async function () {
   const cameraKit = await bootstrapCameraKit({
-    apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzQ3NTk3MzYwLCJzdWIiOiJhNGI3YWI4Yi1mYzU2LTQwOTQtOTU0ZC0xYzgwMTZhYzA0NTl-UFJPRFVDVElPTn5lMzk4NThmNS00MjkzLTRmZTAtOGU4MC0xNTUzMjkzZWI4ZjMifQ.cKda0zovawMjyQo9fjM5LfwyWcYMuTqp1LGizBC727c',
+    apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzQ3NTk3MzYwLCJzdWIiOiJhNGI3YWI4Yi1mYzU2LTQwOTQtOTU0ZC0xYzgwMTZhYzA0NTl-U1RBR0lOR344ODBmYzY5OS03NTZkLTQzYzItODRmMy04M2UzMmRlYWNhMDIifQ.GQRf-5JdZrZNjB1dFXVsELfvTKH7g9XGquWTiexcL-w',
   });
   const liveRenderTarget = document.getElementById(
     'canvas'
